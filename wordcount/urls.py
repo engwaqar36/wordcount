f7afc24759cca669c072a0c8c1f7e47077ba834a
engwaqar36 <37243593+engwaqar36@users.noreply.github.com>
2019-02-03 18:47:21 +0800

    second commit

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.homepage, name='home'),
     path('contact_us/',views.contact),
     path('count/',views.count, name='count')
 ]
